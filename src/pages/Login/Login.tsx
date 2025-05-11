28be7a70a0701775235c3498c9e9593eddb502cd
何金玉 <hejinyuhello@gmail.com>
2025-05-11 12:29:12 +0800

    feat: 重构 List 组件，添加 Home、Login 和 Register 页面，更新样式

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Login: FC = () => {
+  return <div>登录</div>;
+};
+
+export default Login;
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Register: FC = () => {
+  return <div>注册</div>;
+};
+
+export default Register;
